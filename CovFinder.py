2b3a25f994ffcefb9680f027ee5b81a02eb6cd99
Stephane De Cesco <sdecesco@gmail.com>
2019-08-27 16:24:32 +0100

    Update

--- CovFinder.py
+++ CovFinder.py
@@ -20,7 +20,6 @@ from rdkit.Chem import AllChem
 from rdkit.Chem import Descriptors as Desc
 from rdkit.Chem.Scaffolds import MurckoScaffold
 from rdkit import DataStructs
-from rdkit.Chem.Fingerprints import FingerprintMols as fps
 from rdkit.Chem import Draw
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -29,6 +28,8 @@ import numpy as np
 import argparse
 import operator
 import progressbar
+import pandas as pd
+from sklearn.decomposition import PCA
 
 # To remove danger of using input in python2 environments.
 try:
@@ -41,17 +42,94 @@ widgets = [' [', progressbar.Timer(), '] ', progressbar.Bar(), ' [', progressbar
 
 
 def dict_of_cov_group():
-    with open('./covalent_groups_SMARTS_list',
-              'r') as cov_group_list:
-        cov_smarts = {}
-        cov_stats = {}
-        for line in cov_group_list:
-            line_splitted = str(line).split('<>')
-            name = line_splitted[0]
-            smart = line_splitted[1]
-            cov_smarts[name] = smart.strip('\n')
-            cov_stats[name] = 0
-        cov_stats['unknown'] = 0
+    cov_smarts = {}
+    cov_stats = {}
+    cov_group_list = [
+        '1_2_dicarbonyl_oxalyl<>[$([CD3&!R][#6,#8,#7,F,Cl,I,Br]),$([CD3&!R][#1]),$([CD2&!R])](=[OD1&!R])[$([CD3&!R][#6,#8,#7,F,Cl,Br,I]),$([CD3&!R][#1]),$([CD2&!R])]=[OD1&!R]',
+        '4_subst_n_alkyltetrahydropyridines<>[a]C1=CCN([A])CC1', '4_vinyl_pyridine<>c1cnccc1[CX3&!R]=[CX3&!R]',
+        'acetylene_alkyne<>[#1,#6]C#CC',
+        'acrylamide<>[$([NX3H2&!R]),$([NX3H1&!R][c,C]),$([NX3&!R]([c,C])[c,C])][CX3&!R](=[OX1])[CX3&!R]=[$([CX3H2&!R]),$([CX3H1&!R][c,C]),$([CX3H0&!R]([c,C])[c,C])]',
+        'acyclic_acetal<>[#6][OX2&!R][$([CX4&!RH2]),$([CX4&!RH1][#6]),$([CX4&!R]([#6])[#6])][OX2&!R][#6]',
+        'acyclic_acid_halide_acyl_halide<>*[CD3&!R]([F,Cl,I,Br])=[OX1]', 'acyclic_alkyne<>[#6][CX2&!R]#[CX2&!R][#6]',
+        'acyclic_ketone<>[#6&!R][CD3&!R](=[OD1&!R])[#6&!R]', 'acyl_amide<>[C,c][C;!R](=O)[N;!R][C;!R](=O)[C,c]',
+        'acyl_cyanides<>[NX1]#[CX2&!R][CX3&!R](=[OX1])[#6]', 'aldehyde<>[*][CX3&!RH1]=[OD1]',
+        'alkyl_halide_noF<>[CX4&!R][Br,I,Cl]', 'alkynes<>[#6][#6]#[#6][#6,#1]', 'alpha_haloamines<>[F,Cl,I,Br][#7]',
+        'alphahalo_ketone_carbonyl<>[#6][CD3&!c](=[OD1])[CX4&!c][Cl,Br,I,F]',
+        'anhydride<>[OX1]=[CD3]([*])[OD2][CD3](=[OX1])[*]',
+        'aziridine<>N1[$([CX4H2]),$([CX4H1][#6]),$([CX4]([#6])[#6])][$([CX4H2]),$([CX4H1][#6]),$([CX4]([#6])[#6])]1',
+        'azo<>[#6][NX2]=[NX2][#6]', 'azocyanamide<>[N;R0]=[N;R0]C#N',
+        'Benzoxazinones<>[#6]-1=[ND2&!R]-[#6]=[C]-[#6](=[OD1&!R])-[OD2&!R]-1',
+        'beta_heterosubstituted_carbonyl<>[OD1&!R]=[$([CD3&!R]);!$([CD3&!R][#8])]CC([F,Br,I,Cl])',
+        'betalactams<>[OX1&!R]=C1CCN1', 'carbamate_thiocarbamate<>[NX3][CX3&!R](=[OX1,SX1])[OX2&!R,#16&!R][#6]',
+        'carbamic_acid<>[$([NX4+]),$([ND1&!R]),$([NX3&!H2]),$([NX3&!RH1][#6]),$([NX3&!R]([#6])[#6])][CD3&!R](=[OD1&!R])[OX2&!RH1,OX1-]',
+        'carbazide<>O=*N=[N+]=[N-]', 'carbodiimide<>[#6][NX2]=C=[NX2][#6]', 'chloramidine<>[Cl]C([C&R0])=N',
+        'coumarines<>[OX1&!R]=c1ccc2ccccc2o1', 'cyanohydrins<>[ND1&!R]#[CD2&!R][CX4&!R][OD2H1]',
+        'cyanophosphonate<>P(OC)(OC)(=O)C#N', 'cyclopropylamine<>C1CC1[NX3&!R]', 'diazonium<>[c,C][N+&!R]#[N&!R]',
+        'enamine<>[CX3]=[CX3&!R][NX3&!R]', 'epoxide<>[OX2r3]1[#6r3][#6r3]1',
+        'formic_acid_esters<>[CX3H1&!R](=[OX1])[OX2&!R][#6]',
+        'halo_alkene<>[Br,F,I,Cl][$([CX3&!R][#6]),$([CX3&!RH1])]=[$([CX3&!R]([#6])[#6]),$([CX3&!RH1][#6]),$([CX3&!RH2])]',
+        'halo_amine<>[CX4&!R][$([NX3&!RH1]),$([NX3&!R][#6])][Br,F,I,Cl]', 'halopyrimidine<>c1cnc([F,Cl,Br,I])nc1',
+        'hemiaminal<>[OX2&!RH1][$([CX4&!RH2]),$([CX4&!RH1][#6]),$([CX4&!R]([#6])[#6])][$([NX3&!RH2]),$([NX3&!RH1][#6]),$([NX3&!R]([#6])[#6])]',
+        'hemiketal<>[OX2&!RH1][$([CX4&!RH2]),$([CX4&!RH1][#6]),$([CX4&!R]([#6])[#6])][$([OX2&!RH1]),$([OX2&!R][#6])]',
+        'heteroatom_heteroatom_N_N<>[$([#6][NH1;!R]-[NH1;!R][#6]),$([#6][NH1;!R]-[N;!R]([#6])[#6]),$([#6][N;!R]([#6])-[N;!R]([#6])[#6])]',
+        'heteroatom_heteroatom_N_S<>[$([#6][NH1;!R]-[SD2;!R][#6]),$([#6][N;!R]([#6])-[SD2;!R][#6])]',
+        'heteroatom_heteroatom_O_N<>[$([#6][#8;!R][NH1;!R][#6]),$([#6][#8;!R][N;!R]([#6])[#6])]',
+        'heteroatom_heteroatom_S_O<>[#6][SD2;!R]-[#8;!R][#6]', 'heteroatom_heteroatom_S_S<>[#6][SD2;!R][SD2;!R][#6]',
+        'hydantoin<>[OX1]=C1[NX3H1]C(=[OX1])CN1', 'hydrazide<>[NX3&!R][NX3&!R][#6X3](=[OX1])[#6]',
+        'hydrazine<>[#6][NX3&!RH1][NX3&!RH2]', 'hydrazone<>[CX3&!R]=[NX2&!R][NX3&!R]',
+        'hydroxamic_acid<>[CX3&!R;$([H0][#6])](=[OD1])[#7X3;$([H1])][OX2&!RH1,OX1-]',
+        'hydroxylamine<>[c,C;!$([#6]=[#8])][$([NX3&!RH1]),$([NX3&!R][#6])][OX2&!RH1]',
+        'imide<>[C,c][C;!R](=O)[N;!R][C;!R](=O)[C,c]',
+        'imidoyl_halide<>[$([CX3&!RH1]),$([CX3&!RH0][#6])](=[NX2&!R])[F,Br,I,Cl]', 'imine<>[CX3]=[NX2!R]',
+        'isocyanate<>[OD1&!R]=[CD2&!R]=[ND2&!R][#6]', 'isocyanide_isonitrile<>[#6][#7+]#[#6-]',
+        'isothiocyanate<>[SD1&!R]=[CD2&!R]=[ND2&!R][#6]', 'lawesson_reagent_derivative<>[#6]P1(=S)SP(S1)(=S)[#6]',
+        'maleimide<>C1=CC(=[OX1])NC1=[OX1]',
+        'michael_acceptors_double<>[C]=[C][$([#6]=[OX1&!R]),$([#6][#7&!R]),$([#6][#16!R])]',
+        'michael_acceptors_triple<>[#6]#[#6][$([#6]=[OX1&!R]),$([#6][#7&!R]),$([#6][#16!R])]',
+        'nitrile<>[ND1&!R]#[CD2!R][#6,#7,#16]', 'o_thiocarbamate<>[#6][OX2&!R][CX3&!R](=[SX1])[NX3&!R][#6]',
+        'orthoquinone<>[OD1&!R]=C1C(=[OD1&!R])C=CC=C1', 'oxime<>[CX3]=[NX2][OX2H1]',
+        'para_hydroquinone<>[OH]c1ccc([OH])cc1', 'stilbene<>c1ccc([#6&!R]=[#6&!R]c2ccccc2)cc1',
+        'pentafluorophen_ester<>C(=O)(C)Oc1c(F)c(F)c(F)c(F)c1(F)',
+        'perhaloketone<>[#6][CD3&!R](=[OD1&!R])[CD4&!R]([F,Br,I,Cl])([F,Br,I,Cl])[F,Br,I,Cl]',
+        'peroxide<>[#6][#8][#8][#6]', 'phosphorane<>C=P',
+        'phosphoric_acid_or_ester<>[#6,#1][OX2&!R][PX4&!R](=[OX1])([OX2&!R][#6,#1])[OX2&!R][#6,#1]',
+        'polyenes<>[#6]=[CX3&!R][CX3&!R]=[$([CX3&!R][#6&!R]),$([CX3&!RH2])]', 'propiolactone<>[OX1&!R]=C1OCC1',
+        'propiosultone<>[OX1&!R]=[SX4]1(=[OX1&!R])OCCC1', 'quinone<>O=[#6]1[#6]:,=[#6][#6](=O)[#6]:,=[#6]1',
+        's_thiocarbamate<>[#6][SX2&!R][CX3&!R](=[OX1])[NX3&!R][#6]',
+        'sulfone<>[$([#16X4&!R](=[OX1])(=[OX1])([#6])[#6]),$([#16X4+2&!R]([OX1-])([OX1-])([#6])[#6])]',
+        'sulfonic_acid_ester<>[#6][SX4&!R](=[OX1])(=[OX1])[$([OX2&!R][#6,#1]),$([OX1-])]', 'sulfonium<>[SX3+]',
+        'sulfonyl_cyanide<>[#6][SD4&!R,SD4+2&!R](=[OD1&!R])(=[OD1&!R])[CD2&!R]#[NX1]',
+        'sulfonyl_halide<>[#6,#7][#16]([F,Cl,I,Br])(=[#8])=[#8]',
+        'sulfonyl_urea<>[NX3H1][CX3](=[OX1])[NX3H1][SX4](=[OX1])=[OX1]',
+        'sulfoxide<>[$([#16X3](=[OX1])([#6])[#6]),$([#16X3+]([OX1-])([#6])[#6])]', 'sulphanylamino<>[#6][NX3H1][SX2H1]',
+        'terminal_vinyl<>[CX3H1&!R]=[CX3H2&!R]', 'thioacetal<>[#6][CX4&!R]([#16&!R][#6])[#16&!R][#6]',
+        'thioamide<>[#6][CX3&!R](=[SX1&!R])[NX3]([#6])[#6]', 'thiocarbonyl_aromatic<>[C,c]=[SX1&!RH0]',
+        'thioester<>[OX1&!R]=[#6]([#6])[SX2]',
+        'thioic_acid<>[$([CX3]([OX2H1])=S),$([CX3]([SX2H1])=O),$([CX3]([SX2H1])=S)]', 'thioketone<>[SD1&!R]=[#6]',
+        'thiol<>[#6][SH1X2&!R]',
+        'thiourea<>[$([NX3&!RH2]),$([NX3&!RH1][#6]),$([NX3&!R]([#6])[#6])][CX3&!R](=[SX1&!R])[$([NX3&!RH2]),$([NX3&!RH1][#6]),$([NX3&!R]([#6])[#6])]',
+        'triflate<>OS(=O)(=O)C(F)(F)F', 'boronic_acid_ester<>[#6]-[BX3](-[O])-[O]',
+        'vinyl_sulfone<>[#6,#7][SX4](=[OX1])(=[OX1])[C]=[C]',
+        'oxodiazolone_1<>[#8X1&!R]=[#6]-1-[#7]-[#6]=[#7X2]-[#8X2]-1',
+        'cyclic_acetal<>[$([#6r5]),$([#6r6])]-[$([#8r5]),$([#8r6])]-[$([CX4H2]),$([CX4H1][#6]),$([CX4]([#6])[#6])]-[$([#8r5]),$([#8r6])]-[$([#6r5]),$([#6r6])]',
+        'sulfonyl_pyrazole<>[#6]-1=[#7]-[#7](-[#6]=[#6]-1)[S&X4&!R]([#6])(=[O&X1])=[O&X1]',
+        'oxodiazolone_2<>[#8X1&!R]=[#6]-1-[#7]-[#7X2]=[#6]-[#8X2]-1',
+        'sulfonyl_pyrazole_2<>[#7X2]-1=[#6!R2]-[#7](-[#6!R2]=[#6!R2]-1)[S&X4&!R]([#6])(=[O&X1])=[O&X1]',
+        'oxazolone<>[#8X1&!R]=[#6]-1-[#6R1]-[#6R1]=[#7X2]-[#8X2]-1',
+        'oxodiazolone_3<>[#8X1&!R]=[#6]-1-[#7X2]=[#6]-[#7]-[#8X2]-1',
+        'carboxamide_1<>[$([#7H]-[#6]),$([#7](-[#6])[#6])]-[#6X3&!R](=[OX1&!R])-[#7]-1-[#6,#7]=[#6,#7]-[#7,#6]=[#6,#7]-1',
+        'tertiary_halo<>[#6]C([#6])([#6])[Br,I,Cl]',
+        'thiazolidinone_ene<>[#6]=[#6]-1-[#16R1X2]-[#6]-[#7]-[#6X3R1]-1=[OX1&!R]',
+        'thiazolidinedione<>S1C(=[OX1])[N]C(=[OX1])C1', '1_2_thiazol_3_one<>S1[NX3H1]C(=[OX1])C=C1',
+        'thiazolidinone<>[OX1&!R]=[#6]-1-[#7]-[#6]-[#6]-[#16]-1', '2-chloroheteroN<>Cl-[#6&R1]-[#7]',
+        '2-chloroheteroN_2<>Cl-[#6&R1]=[#7]']
+    for line in cov_group_list:
+        line_splitted = str(line).split('<>')
+        name = line_splitted[0]
+        smart = line_splitted[1]
+        cov_smarts[name] = smart.strip('\n')
+        cov_stats[name] = 0
+    cov_stats['unknown'] = 0
     return cov_smarts, cov_stats
 
 
@@ -142,6 +220,7 @@ def generate_stat(new_list):
 
     if not args.no_entropy:
         entropy = get_entropy_measures(new_list)
+        chem_coverage_score = get_chemical_coverage_score(new_list)
 
     for m in new_list:
         descriptors_stat['TPSA'].append(float(m.GetProp('TPSA')))
@@ -191,7 +270,7 @@ def generate_stat(new_list):
     descriptors_stat['HBD'] = np.array(descriptors_stat['HBD'])
 
     if not args.only_pie:
-        gs = gridspec.GridSpec(7, 2)
+        gs = gridspec.GridSpec(8, 2)
         props = dict(boxstyle='round', facecolor='wheat')
 
         plt.figure(1, (20, 20))
@@ -263,7 +342,7 @@ def generate_stat(new_list):
             ax.annotate(round(entropy['SSE_similarity'], 3), ((entropy['SSE_similarity'], 0.5)), color='b',
                         weight='bold',
                         fontsize=20, ha='center', va='center', bbox=props)
-            ax.annotate('no diversity', (0, 0.9), weight='bold',
+            ax.annotate('No diversity', (0, 0.9), weight='bold',
                         fontsize=12, ha='center', va='center', bbox=props)
             ax.annotate('high diversity', (1, 0.9), weight='bold',
                         fontsize=12, ha='center', va='center', bbox=props)
@@ -282,14 +361,32 @@ def generate_stat(new_list):
             ax2.annotate(round(entropy['SSE_scaffold'], 3), ((entropy['SSE_scaffold'], 0.5)), color='b', weight='bold',
                          fontsize=20, ha='center', va='center', bbox=props)
 
-            ax2.annotate('no diversity', (0, 0.9), weight='bold',
+            ax2.annotate('No diversity', (0, 0.9), weight='bold',
                          fontsize=12, ha='center', va='center', bbox=props)
-            ax2.annotate('high diversity', (1, 0.9), weight='bold',
+            ax2.annotate('High diversity', (1, 0.9), weight='bold',
                          fontsize=12, ha='center', va='center', bbox=props)
             cmap = mpl.cm.RdYlGn
             norm = mpl.colors.Normalize(vmin=0, vmax=1)
 
             cb1 = mpl.colorbar.ColorbarBase(ax2, cmap=cmap,
+                                            norm=norm,
+                                            orientation='horizontal')
+            ax3 = plt.subplot(gs[7, :])
+
+            plt.title('Chemical space coverage score', fontsize=20, weight='bold', bbox=props)
+            rect = mpl.patches.Rectangle((chem_coverage_score + 0.005, 0), 1, 0.01, color='b', angle=90.0)
+            ax3.add_patch(rect)
+            ax3.annotate(round(chem_coverage_score, 3), ((chem_coverage_score, 0.5)), color='b', weight='bold',
+                         fontsize=20, ha='center', va='center', bbox=props)
+
+            ax3.annotate('Low coverage', (0, 0.9), weight='bold',
+                         fontsize=12, ha='center', va='center', bbox=props)
+            ax3.annotate('High coverage', (1, 0.9), weight='bold',
+                         fontsize=12, ha='center', va='center', bbox=props)
+            cmap = mpl.cm.RdYlGn
+            norm = mpl.colors.Normalize(vmin=0, vmax=1)
+
+            cb1 = mpl.colorbar.ColorbarBase(ax3, cmap=cmap,
                                             norm=norm,
                                             orientation='horizontal')
 
@@ -301,9 +398,10 @@ def generate_stat(new_list):
         colors = cmap(np.linspace(0, 1, len(values)))
         p, tx, autotexts = plt.pie(values, shadow=True, startangle=90, colors=colors, autopct='%1.1f%%',
                                    counterclock=False)
+        plt.setp(autotexts, size=8, weight="bold")
         for i, a in enumerate(autotexts):
             a.set_text("{}".format(str(a.get_text()) + '\n(' + str(values[i]) + ')'))
-        plt.legend(labels, loc="best")
+        plt.legend(labels, loc="best",fontsize='medium')
         plt.axis('equal')
         plt.title('Covalent groups repartition')
 
@@ -314,9 +412,10 @@ def generate_stat(new_list):
         p, tx, autotexts = plt.pie(values_other, shadow=True, startangle=90, colors=colors,
                                    autopct='%1.1f%%',
                                    counterclock=False)
+        plt.setp(autotexts, size=8, weight="bold")
         for i, a in enumerate(autotexts):
             a.set_text("{}".format('(' + str(values_other[i]) + ')'))
-        plt.legend(labels_other, loc="best")
+        plt.legend(labels_other, loc="best",fontsize='medium')
         plt.axis('equal')
         text = ''
         for i in range(len(labels_other_less)):
@@ -327,13 +426,42 @@ def generate_stat(new_list):
     plt.show()
 
 
+def bin_PCA(df, num_bins=5):
+    pca1 = pd.DataFrame(data=pd.cut(df['PCA 1'], num_bins, labels=False).values, columns=['PCA1_bin'])
+    pca2 = pd.DataFrame(data=pd.cut(df['PCA 2'], num_bins, labels=False).values, columns=['PCA2_bin'])
+    pca3 = pd.DataFrame(data=pd.cut(df['PCA 3'], num_bins, labels=False).values, columns=['PCA3_bin'])
+    pca4 = pd.DataFrame(data=pd.cut(df['PCA 4'], num_bins, labels=False).values, columns=['PCA4_bin'])
+    pca5 = pd.DataFrame(data=pd.cut(df['PCA 5'], num_bins, labels=False).values, columns=['PCA5_bin'])
+    df = df.join(pca1).join(pca2).join(pca3).join(pca4).join(pca5)
+    df['Bin_ID'] = df[['PCA1_bin', 'PCA2_bin', 'PCA3_bin', 'PCA4_bin', 'PCA5_bin']].apply(
+        lambda x: ''.join(map(str, x)), axis=1)
+    return df
+
+
+def get_chemical_coverage_score(mol_list):
+    fps = []
+    for m in mol_list:
+        arr = np.zeros((1,))
+        f = AllChem.GetMorganFingerprintAsBitVect(m, 3)
+        DataStructs.ConvertToNumpyArray(f, arr)
+        fps.append(arr)
+    fps_array = np.asarray(fps)
+    pca = PCA(n_components=5)
+    pca.fit(fps_array)
+    Xred = pca.transform(fps_array)
+    PCA_df = pd.DataFrame(Xred, columns=['PCA 1', 'PCA 2', 'PCA 3', 'PCA 4', 'PCA 5'])
+    frame_PCA = bin_PCA(PCA_df, num_bins=5)
+    score = frame_PCA.Bin_ID.nunique() / (6 ** 5)
+    return score
+
+
 def get_entropy_measures(mol_list):
     list_cores = []
     fps_list = []
     for m in mol_list:
         core_smi = Chem.MolToSmiles(MurckoScaffold.GetScaffoldForMol(m))
         list_cores.append(core_smi)
-        fps_list.append(fps.FingerprintMol(m))
+        fps_list.append(AllChem.GetMorganFingerprintAsBitVect(m, 3))
 
     unique_cores = list(set(list_cores))
     core_count = []
@@ -355,8 +483,7 @@ def get_entropy_measures(mol_list):
         ratio = str(round(core[1] / len(mol_list) * 100, 1))
         legend.append('Count = ' + str(core[1]) + ' (' + ratio + '%)')
 
-    img = Draw.MolsToGridImage(mol_core, molsPerRow=4, subImgSize=(200, 200),
-                               legends=legend, fitImage=True)
+    img = Draw.MolsToGridImage(mol_core, molsPerRow=4, subImgSize=(200, 200), legends=legend)
     img.save(args.output + '_top_cores.png')
 
     s_entropy = 0
@@ -438,8 +565,6 @@ if __name__ == "__main__":
     # mol_list = Chem.SDMolSupplier('data/Enamine_Covalent_Fragments_3038cmpds.sdf')
 
     # mol_list = Chem.SDMolSupplier('data/Enamine_Cys_focused_Covalent_Fragments_995cmpds.sdf')
-
-
 
     new_list = find_covgroup(mol_list)
 
